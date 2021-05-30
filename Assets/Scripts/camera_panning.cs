42b8a718b73da4ac1325cc65bbec898e73fdaf7b
siweisun <siweisun@umich.edu>
2021-05-29 21:42:04 -0400

    Camera moves now I'm not sure why it's not possible to see anything, I'll work on that later.

--- Assets/Scripts/camera_panning.cs
+++ Assets/Scripts/camera_panning.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class camera_panning : MonoBehaviour
 {
-    // Start is called before the first frame update
-    float speed;
-    bool automatic;
+    float speed; // Speed of camera movement
+    bool automatic; // automatic is true when camera is locked on to the player (false when player is manually controlling camera) (we probably don't actually need this)
+    GameObject Player; // Player object
 
+    // Start is called before the first frame update
     void Start()
     {
         speed = 1; // Idk what this should be just testing lmao
         // Add code here to get player object (but we currently do not have a player object so xd)
-        automatic = true; // automatic is true when camera is locked on to the player (false when player is manually controlling camera)
+        // automatic = true;
+        Player = GameObject.Find("Player");
+        if (Player)
+        {
+            //transform.position = Player.transform.position;
+            //transform.position += new Vector3(0, 0, 10); // Camera should be above player
+            Debug.Log(transform.position.x + " " + (transform.position.y + 10) + " " + transform.position.z);
+        }
+        else
+        {
+            Debug.Log("No Player Object");
+            Application.Quit();
+        }
     }
 
     // Update is called once per frame
@@ -23,19 +36,28 @@ public class camera_panning : MonoBehaviour
 
     void FixedUpdate()
     {
+        Debug.Log(transform.position.x + " " + (transform.position.y + 10) + " " + transform.position.z);
         if (Input.GetKey(KeyCode.S))
         {
-            Debug.Log("S key is held down");
+            // Debug.Log("S key is held down");
             transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
         }
         else if (Input.GetKey(KeyCode.W))
         {
-            Debug.Log("W key is held down");
+            // Debug.Log("W key is held down");
             transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
         }
+        //else if (Input.GetKey(KeyCode.D))
+        //{
+        //    // Debug.Log("D key is held down");
+        //    transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        //}
         else
         {
             // Snap back to player
+            // TODO: Make this move smoothly, not instantaneously
+            transform.position = Player.transform.position;
+            transform.position += new Vector3(0, 0, 10); // Camera should be above player
         }
     }
 }
