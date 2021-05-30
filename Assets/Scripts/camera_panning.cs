e71f7f84aeb730fc1ae7a74e72c18d18f2d241be
siweisun <siweisun@umich.edu>
2021-05-29 21:08:52 -0400

    can move camera now, waiting for player object/movement

--- Assets/Scripts/camera_panning.cs
+++ Assets/Scripts/camera_panning.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class camera_panning : MonoBehaviour
 {
     // Start is called before the first frame update
+    float speed;
+    bool automatic;
+
     void Start()
     {
-        
+        speed = 1; // Idk what this should be just testing lmao
+        // Add code here to get player object (but we currently do not have a player object so xd)
+        automatic = true; // automatic is true when camera is locked on to the player (false when player is manually controlling camera)
     }
 
     // Update is called once per frame
@@ -21,10 +26,16 @@ public class camera_panning : MonoBehaviour
         if (Input.GetKey(KeyCode.S))
         {
             Debug.Log("S key is held down");
+            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
         }
         else if (Input.GetKey(KeyCode.W))
         {
             Debug.Log("W key is held down");
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+        else
+        {
+            // Snap back to player
         }
     }
 }
