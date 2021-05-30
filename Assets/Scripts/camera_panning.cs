660d84db11dc7637ed2945ac90eab6014f2c2b8b
siweisun <siweisun@umich.edu>
2021-05-29 22:14:50 -0400

    camera now has a max distance that it can travel

--- Assets/Scripts/camera_panning.cs
+++ Assets/Scripts/camera_panning.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO: Limit the distance that the camera can be moved
+
 public class camera_panning : MonoBehaviour
 {
     float speed; // Speed of camera movement
     float transitionSpeed; // Speed of snapback
+    float maxDist; // Maximum distance the camera can travel
     bool automatic; // automatic is true when camera is locked on to the player (false when player is manually controlling camera) (we probably don't actually need this)
     GameObject Player; // Player object
 
@@ -14,6 +17,7 @@ public class camera_panning : MonoBehaviour
     {
         speed = 3; // Idk what this should be just testing
         transitionSpeed = 10;
+        maxDist = 3;
         Player = GameObject.Find("Player");
         if (Player)
         {
@@ -39,13 +43,27 @@ public class camera_panning : MonoBehaviour
         // Debug.Log(transform.position.x + " " + (transform.position.y - 10) + " " + transform.position.z);
         if (Input.GetKey(KeyCode.S))
         {
-            // Debug.Log("S key is held down");
-            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+            if (Mathf.Abs(transform.position.y - Player.transform.position.y) < maxDist)
+            {
+                // Debug.Log("S key is held down");
+                transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+            }
+            else
+            {
+                // Debug.Log("Camera has gone too far");
+            }
         }
         else if (Input.GetKey(KeyCode.W))
         {
-            // Debug.Log("W key is held down");
-            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+            if (Mathf.Abs(transform.position.y - Player.transform.position.y) < maxDist)
+            {
+                // Debug.Log("W key is held down");
+                transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+            }
+            else
+            {
+                // Debug.Log("Camera has gone too far");
+            }
         }
         //else if (Input.GetKey(KeyCode.D))
         //{
