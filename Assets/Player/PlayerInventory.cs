53806d63745ec0f7b74ba1600b5589ad7d24e0c8
Danni Hu <dannihu@gmail.com>
2021-05-31 12:32:11 -0400

    Add basic blinking but blinks way too quickly
    Can someone fix this?

--- Assets/Player/PlayerInventory.cs
+++ Assets/Player/PlayerInventory.cs
@@ -1,46 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInventory : MonoBehaviour
 {
-    public int m_MaxSwords = 3; // Max number of swords player can carry
-    int cur_swords = 0; // Current number of swords on player
+    public int cur_swords = 0; // Current number of swords on player
+    private float blink_time = 5f; // blinking time
     Camera m_MainCamera;
-    // Start is called before the first frame update
 
-    // create canvas, draw directly in front of camera
+    public Text swordText;
+  
+    
+    // Start is called before the first frame update
 
     void Start()
     {
         m_MainCamera = Camera.main;
+        swordText.text = "Swords: " + cur_swords;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
+        //OnTriggerEnter2D()
     }
 
     void FixedUpdate()
     {
+        StartBlinking();
+    }
+
+    void updateSwordText()
+    {
+        swordText.text = "Swords: " + cur_swords;
 
     }
+
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Sword")
         {
-            Destroy(other.gameObject);
-            if (cur_swords < m_MaxSwords)
+            Destroy(other.gameObject); // change to make swords floating down?
+            ++cur_swords;
+        }
+    }
+
+    IEnumerator Blink()
+    {
+        while (cur_swords < 2)
+        {
+            switch (swordText.color.a.ToString())
             {
-                ++cur_swords;
-                Debug.Log("Sword Picked Up");
-            }
-            else
-            {
-                Debug.Log("Unable to pick up sword, inventory full");
+                case "0":
+                    swordText.color = new Color(swordText.color.r, swordText.color.g, swordText.color.b, 1);
+                    yield return new WaitForSeconds(blink_time);
+                    break;
+                case "1":
+                    swordText.color = new Color(255, 0, 0, 0);
+                    yield return new WaitForSeconds(blink_time);
+                    break;
+
             }
         }
+    }
+
+    void StartBlinking()
+    {
+        StopCoroutine("Blink");
+        StartCoroutine("Blink");
+    }
+
+    void StopBlinking()
+    {
+        StopCoroutine("Blink");
     }
 }
